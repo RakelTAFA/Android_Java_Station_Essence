634e16bf793a994c4a6ebf403dc73903e41466b0
RakelTAFA <rakel.tafa@etu.univ-lyon1.fr>
2023-03-26 22:20:11 +0200

    Page de lancement (à tester)

---
+++ app/src/main/java/com/example/androidprojectgasstations/LauncherActivity.java
@@ -0,0 +1,34 @@
+package com.example.androidprojectgasstations;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.widget.ImageView;
+
+public class LauncherActivity extends AppCompatActivity {
+
+    private final int SPLASH_SCREEN_TIMEOUT = 1000;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_launcher);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(LauncherActivity.this, GasStationsActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        }, SPLASH_SCREEN_TIMEOUT);
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        getWindow().getDecorView().removeCallbacks(null);
+    }
+}
