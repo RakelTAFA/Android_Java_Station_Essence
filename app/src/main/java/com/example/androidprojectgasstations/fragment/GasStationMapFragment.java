a2a4b8c4c1a664fa0ba2623bbca3fa5a9c1699f2
RakelTAFA <rakel.tafa@etu.univ-lyon1.fr>
2023-03-28 23:45:26 +0200

    Début map

--- app/src/main/java/com/example/androidprojectgasstations/fragment/GasStationMapFragment.java
+++ app/src/main/java/com/example/androidprojectgasstations/fragment/GasStationMapFragment.java
@@ -1,9 +1,15 @@
 package com.example.androidprojectgasstations.fragment;
 
+import android.content.Context;
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentActivity;
+import androidx.fragment.app.FragmentTransaction;
 
+import android.util.AttributeSet;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -13,6 +19,9 @@ import com.example.androidprojectgasstations.manager.CacheManager;
 import com.example.androidprojectgasstations.model.GasStation;
 import com.example.androidprojectgasstations.model.GasStationFields;
 import com.example.androidprojectgasstations.model.GasStationGeometry;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
 
 import java.math.BigDecimal;
 import java.util.ArrayList;
@@ -21,15 +30,25 @@ public class GasStationMapFragment extends Fragment {
 
     View view;
     GasStation gasStation = null;
-
+    private GoogleMap gMap;
 
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         view = inflater.inflate(R.layout.fragment_gas_station_map, container, false);
 
+        SupportMapFragment mapFragment = SupportMapFragment.newInstance();
+        FragmentTransaction fragmentTransaction = getChildFragmentManager().beginTransaction();
+        fragmentTransaction.add(R.id.map_widget, mapFragment);
+        fragmentTransaction.commit();
 
+        mapFragment.getMapAsync(new OnMapReadyCallback() {
+            @Override
+            public void onMapReady(GoogleMap googleMap) {
+                gMap = googleMap;
+                // ...
+            }
+        });
 
         ArrayList<GasStationGeometry> gasStationGeometry = new ArrayList<GasStationGeometry>();
 
